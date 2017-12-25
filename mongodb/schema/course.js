0e72ec7968f8548d4d8401fb0a74cbd4708e9a9f
naihe138 <370215230@qq.com>
2017-12-25 09:07:48 +0800

    课程schema

---
+++ mongodb/schema/course.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+
+const StudentSchema = new Schema({
+  title: String,
+  desc: String,
+  page: Number,
+  author: String,
+  meta: {
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
+
+StudentSchema.pre('save', function (next) {
+  if (this.isNew) {
+    this.meta.createdAt = this.meta.updatedAt = Date.now()
+  } else {
+    this.meta.updatedAt = Date.now()
+  }
+
+  next()
+})
+
+mongoose.model('Student', StudentSchema)
